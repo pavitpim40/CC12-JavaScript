c80f52b00a978bdcd1767c4edd875b3ebf89716c
Pavit <pavitpim@gmail.com>
2022-07-28 13:07:54 +0700

    update:lab function

--- _instructor/04-Condition/03-LogicalOperator/_demo/main.js
+++ _instructor/04-Condition/03-LogicalOperator/_demo/main.js
@@ -14,3 +14,27 @@ username.trim() && alert(username)
 // "John" && alert("John")
 // null && alert() // not alert
 // "" && alert() // not alert
+
+
+// ###################################
+// OR ==> First truthy
+// let a = '' || "CC" || null || 50
+// a
+
+// OR ==> Last (all false)
+// let b = '' || undefined || null
+// b
+
+// let a = '' && undefined && 0;
+// let b = undefined && 0 && "";
+// console.log(b)
+// Boolean("")
+// Boolean(" ")
+
+// null === null
+
+// Number("Codecamp")
+
+isNaN(NaN)
+isNaN("0")
+isNaN("C  C")
