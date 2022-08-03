9a629287d06ea2b245afcf9b4efe10ec307e7b89
Pavit <pavitpim@gmail.com>
2022-08-03 15:15:40 +0700

    update demo:basicArray

--- _instructor/09-Array/04-loop-array/_demo/main.js
+++ _instructor/09-Array/04-loop-array/_demo/main.js
@@ -1,70 +1,24 @@
 
-// ### FOR
-// let arr = ['Apple', 'Orange', 'Pear'];
-
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
 
 
-// FOR IN
-// let arr = ['Apple', 'Orange', 'Pear'];
-
-// for (let index in arr) {
-//   console.log(arr[index]);
-// }
-
-
-// FOR OF 
-// let arr = ['Apple', 'Orange', 'Pear'];
-
-// for (let item of arr) {
-//   console.log(item);
-// }
-
-
-// FOREACH : Array Method
-
-const arr = ['Cat',"Dog","Rat","Hippo"]
-// DECLARATION
-function logName(a,b,c) {
-    console.log(a,b,c)
-}
-
-// function logNameWithConvention(item,index,array) {
-//     console.log(item,index,array)
-// }
-
-// Expression
-// let logNameWithConvention = function(item,index,array) {
-//     console.log(item,index,array)
-// }
-
-// Arrow
-let logNameWithConvention = (item,index,array) => {
-     console.log(item,index,array)
- }
-
-function alertName(d,e,f) {
-    alert(d)
-}
-
-// arr.forEach(logNameWithConvention)
-// arr.forEach(alertName)
-
-// ### PASS CALLBACK ON THE FLY
-// arr.forEach(function (item,index,arr){
-//     console.log(item)
-// })
-
-// ### RETURN VALUE
-// let afterForEach = arr.forEach(function (item,index,arr){
-//     console.log(item)
-// })
-
-// console.log(afterForEach)
-
-['Bilbo', 'Gandalf', 'Nazgul'].forEach((item, index, array) => {
-    console.log(`${item} is at index ${index} in ${array}`);
-});
+// function myFunc (a,b,c) {
+// //  a , b ==> Pass Primitive Value : number,string,boolean,null,undefied
   
+// //   c ==> Pass Function เพื่อให้ myFunc เรียกใช้งานใน scope ได้
+// //   c ==> Callback
+//   console.log(a,b,c)
+//   c() // call
+// }
+
+// let AA = "Hi"
+// let BB = 37
+// let CC = function () {
+//   console.log("EXECUTE BY PARAMETER,CALLBACK")
+// }
+// let DD = function () {
+//   console.log("ANOTHER CALLBACK")
+// }
+
+// myFunc(AA,BB,CC)
+// myFunc(AA,BB,DD)
+
