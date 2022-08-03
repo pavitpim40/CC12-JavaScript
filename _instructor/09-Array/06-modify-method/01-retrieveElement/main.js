605803ec7b12ceb70dd79c26b7da84226aaeda53
Pavit <pavitpim@gmail.com>
2022-08-03 17:03:08 +0700

    update demo:search,transform

---
+++ _instructor/09-Array/06-modify-method/01-retrieveElement/main.js
@@ -0,0 +1,7 @@
+const sports = ['football', 'volleyball', 'tennis', 'basketball', 'judo'];
+
+const res = sports.splice(1,3)
+// const res = sports.slice(1,4)
+
+console.log("ORG",sports)
+console.log("NEW",res)
---
+++ _instructor/09-Array/06-modify-method/02-removeFish/main.js
@@ -0,0 +1,9 @@
+let fish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+
+//const res  = fish.splice(0,4) // it's work
+// const res = fish.splice(0) // still work !
+// const res = fish.splice() // doesnt work
+
+
+console.log("ORG",fish)
+console.log("NEW",res)
